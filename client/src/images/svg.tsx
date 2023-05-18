2ccdad741ce2c68c1b03575673acf14a736bc7cb
WilerMS <wilersoriano@gmail.com>
2023-05-18 23:58:02 +0200

    images

---
+++ client/src/images/svg.tsx
@@ -0,0 +1,24 @@
+export const HomeSectionImage3 = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" id="visual" viewBox="0 0 900 600" version="1.1">
+    <g>
+      <g transform="translate(683 88)">
+        <path d="M0 -132.4L114.7 -66.2L114.7 66.2L0 132.4L-114.7 66.2L-114.7 -66.2Z" fill="#4ade80" />
+      </g>
+      <g transform="translate(759 367)">
+        <path d="M0 -117L101.3 -58.5L101.3 58.5L0 117L-101.3 58.5L-101.3 -58.5Z" fill="#4ade80" />
+      </g>
+      <g transform="translate(221 518)">
+        <path d="M0 -120L103.9 -60L103.9 60L0 120L-103.9 60L-103.9 -60Z" fill="#4ade80" />
+      </g>
+      <g transform="translate(125 171)">
+        <path d="M0 -69L59.8 -34.5L59.8 34.5L0 69L-59.8 34.5L-59.8 -34.5Z" fill="#4ade80" />
+      </g>
+      <g transform="translate(533 503)">
+        <path d="M0 -118L102.2 -59L102.2 59L0 118L-102.2 59L-102.2 -59Z" fill="#4ade80" />
+      </g>
+      <g transform="translate(387 112)">
+        <path d="M0 -69L59.8 -34.5L59.8 34.5L0 69L-59.8 34.5L-59.8 -34.5Z" fill="#4ade80" />
+      </g>
+    </g>
+  </svg>
+)
