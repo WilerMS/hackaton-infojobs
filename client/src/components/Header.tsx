ca1cfd921e11c27d32bced0823c3940fe252fd73
WilerMS <wilersoriano@gmail.com>
2023-05-21 00:39:57 +0200

    Header tyles

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className='fixed top-0 h-[80px] w-full z-50 bg-white flex items-center justify-center border-b border-gray-50'>
+    <header className='fixed top-0 h-[80px] w-full z-50 bg-white flex items-center justify-center'> {/* border-b border-gray-50 */}
       <div className="w-full h-full contained flex justify-between items-center">
         <div className="logo flex items-center cursor-pointer">
           <span className='text-xl font-bold px-2 py-1 rounded-lg text-white uppercase bg-green-400'>Just</span>
