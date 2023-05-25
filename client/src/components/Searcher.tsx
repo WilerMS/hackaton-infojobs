4e3eac5704949228528822ec8b398d819a2d01c8
WilerMS <wilersoriano@gmail.com>
2023-05-25 23:15:10 +0200

    Responsive and event to Searcher

--- client/src/components/Searcher.tsx
+++ client/src/components/Searcher.tsx
@@ -1,28 +1,45 @@
-import { FaMapMarkerAlt, FaSearch } from 'react-icons/fa'
+import { useState, type FC } from 'react'
+import { FaSearch } from 'react-icons/fa'
 
-const Searcher = () => {
+interface Props {
+  value?: string
+  onChange?: (value: string) => void
+  onClickSearch?: (value: string) => void
+}
+
+const Searcher: FC<Props> = ({
+  value = '',
+  onChange = () => { },
+  onClickSearch = () => { }
+}) => {
+  const [text, setText] = useState<string>(value)
+
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setText(e.currentTarget.value)
+    onChange(e.currentTarget.value)
+  }
+
+  const handleClick = () => onClickSearch(text)
+
   return (
     <div className='w-full h-16 rounded-lg border flex overflow-hidden hover:shadow-sm transition'>
       <div className="w-full h-full px-5 flex items-center gap-3">
         <FaSearch className='text-lg text-gray-400' />
         <input
+          value={text}
           type="text"
-          className="border-none outline-none w-full h-full placeholder:text-sm"
+          className="w-full h-full placeholder:text-sm outline-disabled"
           placeholder="Search job title, keyword or url"
+          onChange={handleChange}
         />
       </div>
-      <div className="w-full h-full border-l pl-5 flex items-center gap-3">
-        <FaMapMarkerAlt className='text-lg text-gray-400' />
-        <input
-          type="text"
-          className="border-0 outline-none w-full h-full placeholder:text-sm focus:border-0 focus:outline-0"
-          placeholder="Country or location"
-        />
-        <div className='w-[150px] p-2 h-full'>
-          <button className='w-full bg-green-600 h-full rounded-md font-bold text-white'>
-            Find Jobs
-          </button>
-        </div>
+      <div className='w-[150px] p-2 h-full'>
+        <button
+          onClick={handleClick}
+          className='w-full bg-green-500 hover:bg-green-600 transition h-full rounded-md font-bold text-white'
+        >
+          Find Jobs
+        </button>
       </div>
     </div>
   )
