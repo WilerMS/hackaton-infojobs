e6ba8e7cf252ff3be917515003a1bf33d50c47da
WilerMS <wilersoriano@gmail.com>
2023-05-26 02:03:56 +0200

    Pagination working

--- client/src/components/Pagination.tsx
+++ client/src/components/Pagination.tsx
@@ -1,39 +1,49 @@
 import { type FC } from 'react'
 import { FaAngleLeft, FaAngleRight } from 'react-icons/fa'
 
-interface PaginationButtonProps {
-  children: JSX.Element | JSX.Element[] | string
+interface PaginationButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: JSX.Element | JSX.Element[] | string | number
 }
 
-export const PaginationButton: FC<PaginationButtonProps> = ({ children }) => {
+interface PaginationProps {
+  page: number
+  onChangePage: (page: number) => void
+}
+
+export const PaginationButton: FC<PaginationButtonProps> = ({ children, ...props }) => {
   return (
-    <button className="h-[40px] w-[40px] rounded-lg border shadow-sm flex items-center justify-center hover:scale-105 transition">
+    <button {...props} className="h-[40px] w-[40px] rounded-lg border shadow-sm flex items-center justify-center hover:scale-105 transition">
       <span className="text-gray-500">{children}</span>
     </button>
   )
 }
 
-export const Pagination = () => {
+export const Pagination: FC<PaginationProps> = ({
+  page,
+  onChangePage
+}) => {
+  const goToPreviousPage = () => {
+    if (page > 1) {
+      onChangePage(page - 1)
+    }
+  }
+
+  const goToNextPage = () => {
+    onChangePage(page + 1)
+  }
+
   return (
     <div className='w-full h-[80px] pt-3 flex items-end justify-center gap-3'>
 
-      <PaginationButton>
+      <PaginationButton onClick={goToPreviousPage}>
         <FaAngleLeft />
       </PaginationButton>
 
       <PaginationButton>
-        1
+        { page }
       </PaginationButton>
 
-      <PaginationButton>
-        2
-      </PaginationButton>
-
-      <PaginationButton>
-        3
-      </PaginationButton>
-
-      <PaginationButton>
+      <PaginationButton onClick={goToNextPage}>
         <FaAngleRight />
       </PaginationButton>
 
