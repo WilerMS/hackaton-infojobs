ccb4ff100026d22964c9a025cfe59c9169b4d29b
WilerMS <wilersoriano@gmail.com>
2023-05-26 02:05:33 +0200

    page, filters, search integrated

--- client/src/pages/HomePage/index.tsx
+++ client/src/pages/HomePage/index.tsx
@@ -1,25 +1,74 @@
 import { Banner } from '@app/components/Banner'
+import Error from '@app/components/Error'
 import Filters from '@app/components/Filters'
 import { JobCard } from '@app/components/JobCard'
+import Loading from '@app/components/Loading'
 import { Pagination } from '@app/components/Pagination'
+import { useOffers } from '@app/hooks/useOffers'
 import Searcher from '@components/Searcher'
+import { type Key, useState, useEffect } from 'react'
 
 const HomePage = () => {
+  const [page, setPage] = useState(1)
+  const [filters, setFilters] = useState({})
+  const [searchText, setSearchText] = useState('')
+  const { offers, isFetching, isError } = useOffers({
+    // @ts-expect-error
+    _page: page,
+    _limit: 4,
+    ...filters
+  })
+
+  const handleChangeFilters = (newFilters = {}) => {
+    setFilters({
+      ...filters,
+      ...newFilters
+    })
+  }
+
+  useEffect(() => {
+    handleChangeFilters({
+      q: searchText
+    })
+  }, [searchText])
+
   return (
-    <main className='w-full sticky -top-[250px] h-[calc(100%+250px)]'>
+    <main className='w-full md:sticky md:-top-[250px] md:h-[calc(100%+250px)]'>
       <Banner />
-      <section className='contained py-5 w-full h-[calc(100%-390px)] flex flex-col md:flex-row gap-4 m-auto'>
-        <Filters />
+      <section className='contained py-5 w-full md:h-[calc(100%-390px)] flex flex-col md:flex-row gap-4 m-auto'>
+        <Filters filters={filters} onChangeFilters={handleChangeFilters} />
         <div className='h-full w-full md:w-[calc(100%-320px-1rem)]'>
-          <Searcher />
+          <Searcher onClickSearch={setSearchText}/>
           <div className='h-12 pb-1 pl-1 flex items-end'>250 Jobs results</div>
           <div className='flex flex-col gap-3 h-[calc(100%-7rem-80px)] md:h-[calc(100%-7rem-80px)]'>
-            <JobCard />
-            <JobCard />
-            <JobCard />
-            <JobCard />
+            {
+              (() => {
+                if (offers) {
+                  return offers.map((offer: Offer) => (
+                    <JobCard
+                      key={offer.id}
+                      address={`${offer.province.value}, ${offer.city}`}
+                      company={offer.author.name}
+                      contractType={offer.contractType.value}
+                      workDay={offer.workDay.value}
+                      date={offer.published}
+                      salary={offer.salaryDescription}
+                      title={offer.title}
+                      companyImage={offer.author?.logoUrl}
+                      content={offer.requirementMin}
+                    />
+                  ))
+                }
+                if (isFetching) return <Loading />
+                if (isError) return <Error />
+                return 'No hay Ofertas'
+              })()
+            }
           </div>
-          <Pagination />
+          <Pagination
+            page={page}
+            onChangePage={setPage}
+          />
         </div>
       </section>
       <footer className='h-[60px] w-full bg-white border-t flex justify-center'>
