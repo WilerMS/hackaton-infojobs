bc3425bf69b66a484bcc552005c807f86fcac6d6
WilerMS <wilersoriano@gmail.com>
2023-05-26 02:04:09 +0200

    Job card working

--- client/src/components/JobCard.tsx
+++ client/src/components/JobCard.tsx
@@ -1,32 +1,60 @@
 import twitter from '@images/twitter.png'
+import { type FC } from 'react'
 import { FaMapMarkerAlt } from 'react-icons/fa'
 
-export const JobCard = () => {
+interface Props {
+  title?: string
+  address?: string
+  company: string
+  date?: string
+  companyImage?: string
+  contractType: string
+  salary: string
+  experienceMin?: string
+  workDay: string
+  content?: string
+}
+
+export const JobCard: FC<Props> = ({
+  title,
+  address,
+  date,
+  company,
+  companyImage,
+  contractType,
+  salary,
+  experienceMin,
+  workDay,
+  content = ''
+}) => {
   return (
     <div className='relative border w-full bg-white rounded-lg p-4 h-[25%] overflow-hidden shadow-sm hover:shadow-md transition cursor-pointer'>
       <div className="flex md:justify-between items-center mb-3">
-        <div className="w-[45px]">
-          <img src={twitter} alt="Logo of twitter" />
+        <div className="w-[45px] h-[45px]">
+          <img src={companyImage ?? twitter} alt="Logo of twitter" />
         </div>
 
         <div className="w-full md:w-[calc(100%-50px-300px)] flex flex-col px-3">
-          <h3 className='font-bold'>Product Designer</h3>
-          <span className='text-sm text-gray-600'>Twitter * Full-time</span>
+          <h3 className='font-bold truncate'>{title}</h3>
+          <span
+            title={`${company} * ${contractType} * ${salary} * ${workDay}`}
+            className='text-sm text-gray-600 truncate'
+          >
+            {company} * {contractType} * {salary} * {workDay}
+          </span>
         </div>
 
         <div className="hidden md:block w-[300px] h-[40px] text-right">
           <div className="flex justify-end items-center gap-1">
             <FaMapMarkerAlt className='text-sm' />
-            <h3>Alcobendas, Madrid Spain</h3>
+            <h3 className='truncate'>{address}</h3>
           </div>
-          <span className='text-sm text-gray-600'>Porsted 5 minutes ago</span>
+          <span className='text-sm text-gray-600'>{date}</span>
         </div>
       </div>
 
       <div className="flex justify-between items-center px-1">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Quos molestias beatae impedit corrupti, ducimus tempore delectus consequuntur nulla molestiae,
-        provident totam cum velit facilis dignissimos.
+        {(content || 'No hay descripción').substring(0, 150)}
       </div>
     </div>
   )
